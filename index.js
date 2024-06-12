40f6bf55af58957740a1910e9b25c893526db3a0
Jan-Frederic Daniel <janfredericdaniel@gmail.com>
2024-06-12 15:41:20 +1000

    Reading and Writing files, Section 2 Introduction to Node.js and npm

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const fs = require('fs');
 
+//reading content of file, takes filepath and character encoding param
+const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
 
-const hello = "Hello World";
-console.log(hello);
+console.log(textIn);
+
+const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
+
+//writing content to a file, takes filepath and content to add. function returns nothing
+fs.writeFileSync('./txt/output.txt', textOut);
+
+console.log("Files written!");
