0852d9e200f9242af26f7f4d49744d2ce92eef8b
Jan-Frederic Daniel <janfredericdaniel@gmail.com>
2024-06-12 17:01:09 +1000

    Creating a simple Web Server, Section 2 Introduction to Node.js and npm

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const fs = require('fs');
+const http = require('http');
+
+
+////////////////////////////////////////
+// FILES
 
 // //reading content of file, takes filepath and character encoding param
 // //Blocking, synchronous way
@@ -15,16 +20,30 @@ const fs = require('fs');
 
 
 //Non-Blocking, asynchronous way
-fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
-    if(err) return console.log("ERROR!");
-    fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
-        console.log(data2);
-        fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
-            console.log(data3);
-            fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
-                console.log("Your file has been written!");
-            })
-        });
-    })
+// fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+//     if(err) return console.log("ERROR!");
+//     fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+//         console.log(data2);
+//         fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
+//             console.log(data3);
+//             fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
+//                 console.log("Your file has been written!");
+//             })
+//         });
+//     })
+// });
+// console.log("Will read file!");
+
+
+////////////////////////////////////////
+// SERVER
+
+//creating server, callback will be executed each time a new request hits server
+const server = http.createServer((req, res) => {
+    res.end('Hello from the server!');
 });
-console.log("Will read file!");
+
+//listening on incoming requests on localhost:8000
+server.listen(8000, '127.0.0.1', () => {
+    console.log('Listening to requests on port 8000');
+});
