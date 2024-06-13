3a3718d31dd0e0a45c2df0f8dbd8410874e6e33e
Jan-Frederic Daniel <janfredericdaniel@gmail.com>
2024-06-13 10:33:08 +1000

    Routing, Section 2 Introduction to Node.js and npm

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const http = require('http');
+const url = require('url');
 
 
 ////////////////////////////////////////
@@ -40,10 +41,24 @@ const http = require('http');
 
 //creating server, callback will be executed each time a new request hits server
 const server = http.createServer((req, res) => {
-    res.end('Hello from the server!');
+    // ROUTING
+    const pathName = req.url;
+
+    if(pathName === '/' || pathName === '/overview') {
+        res.end('This is the OVERVIEW');
+    } else if (pathName === '/product') {
+        res.end('This is the PRODUCT');
+    } else {
+        //sending a html header element to the browser
+        res.writeHead(404, {
+            'Content-type': 'text/html',
+            'my-own-header': 'hello-world'
+        });
+        res.end('<h1>Page not found!</h1>');
+    }
 });
 
-//listening on incoming requests on localhost:8000
+//listening for incoming requests on localhost:8000
 server.listen(8000, '127.0.0.1', () => {
     console.log('Listening to requests on port 8000');
 });
